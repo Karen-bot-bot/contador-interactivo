7ee5208b0aed25b993c10bad752ccc924a00f14e
Karen Luna <karenlunafeldheim@gmail.com>
2025-02-22 11:57:25 -0300

    Funcionalidad de los botones

---
+++ src/script.js
@@ -0,0 +1,21 @@
+let contador = 0;
+
+const numeroContador = document.querySelector("#numero-contador");
+const btnAumentar = document.querySelector("#boton-aumentar");
+const btnDisminuir = document.querySelector("#boton-disminuir");
+const btnReset = document.querySelector("#boton-reiniciar");
+
+btnAumentar.addEventListener("click", function () {
+  contador++;
+  numeroContador.textContent = contador; // Actualizamos el número en pantalla
+});
+
+btnDisminuir.addEventListener("click", function () {
+  contador--;
+  numeroContador.textContent = contador;
+});
+
+btnReset.addEventListener("click", function () {
+  contador = 0;
+  numeroContador.textContent = "0";
+});
